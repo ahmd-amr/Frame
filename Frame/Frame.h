8ade819bf8b509a15bbeb55c96be2cc69a7d4f60
Ahmed Amr <ahmed.amr@fawry.com>
2022-10-02 10:59:49 +0200

    Initial Commit

---
+++ Frame/Frame.h
@@ -0,0 +1,18 @@
+//
+//  Frame.h
+//  Frame
+//
+//  Created by Ahmed Sharf on 02/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Frame.
+FOUNDATION_EXPORT double FrameVersionNumber;
+
+//! Project version string for Frame.
+FOUNDATION_EXPORT const unsigned char FrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frame/PublicHeader.h>
+
+
